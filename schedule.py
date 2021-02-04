8924da85820300b2040e3168251812697163df00
nodri09 <nodar01@gmail.com>
2021-02-04 13:39:17 +0300

    recompiled program without terminal at the background

--- schedule.py
+++ schedule.py
@@ -5,31 +5,17 @@ Created by Gelovani Nodar
 import openpyxl as xl
 from set_calc import yeardays, get_shiftset, Manager, printff
 
-# Check if command line arguments are 3
-# Check if second and third files have 'xlsx' file extention
-# If not correct any of the above, exit
-"""
-if len(argv) == 3:
-    if argv[1].endswith('xlsx') and argv[2].endswith('xlsx'):
-      printff("Starting the program...\n")
-    else:
-      printff("Incorrect file extention\n")
-      exit()
-else:
-    printff("Something went wrong. Cannot open the file.\n")
-    exit()
-"""
 
 def main(manager_file, yearInput, monthInput, teams_file):
 
 
     # Transfer data from Excel file
     # Open Excel Workbook
-    printff(f"Opening file: {manager_file}\n")
+    
     wb = xl.load_workbook(manager_file)
 
     # Open Active worksheet
-    printff("Opening active worksheet...\n")
+    
     sheet = wb.active
 
     # List of Manager class - All the managers in one List
@@ -39,7 +25,7 @@ def main(manager_file, yearInput, monthInput, teams_file):
     max_row = sheet.max_row
 
     # Transfer information from Managers excel
-    printff("Retreiving information from columns...\n")
+    
     for row in range(2, max_row + 1):
         name = sheet['A' + str(row)].value
         shift_type = sheet['B' + str(row)].value
@@ -54,7 +40,7 @@ def main(manager_file, yearInput, monthInput, teams_file):
 
     # Ask user to provide Year and Month
     # Check to be numbers only
-    printff("Dictate the year:\n ")
+    
     user_year = yearInput
 
     while False:
@@ -63,7 +49,7 @@ def main(manager_file, yearInput, monthInput, teams_file):
       else:
         True
 
-    printff("Dictate the month:\n ")
+    
     user_month = monthInput
 
     while False:
@@ -73,9 +59,9 @@ def main(manager_file, yearInput, monthInput, teams_file):
         True
 
     year = user_year
-    printff(f"Year is: {year}\n")
+    
     month = user_month
-    printff(f"Month is: {month}\n")
+    
 
     # print("\nJan.'21, Set 1")
     # print([str(day) for day in yeardays(year) if day.month == month and get_shiftset(day)=="Set 1"])
@@ -84,7 +70,7 @@ def main(manager_file, yearInput, monthInput, teams_file):
 
     # Generate set days of the given month of the given year
     # Assign generated set-days to managers, which have the same set
-    printff("Generating set days for each Manager\n")
+    
     for manager in managers:
         for day in yeardays(year):
             if day.month == month and get_shiftset(day) == "Set 1" and manager.mset == 1:
@@ -100,13 +86,13 @@ def main(manager_file, yearInput, monthInput, teams_file):
 
     # Open Team's schedule template file
     wb1 = xl.load_workbook(teams_file)
-    printff(f"Opening file: {teams_file}\n")
+    
 
     # Load 'Shifts' sheet
     tsheet = wb1['Shifts']
 
     # Translateing managers info to excel column
-    printff("Translateing managers info to excel column\n")
+    
     for manager in managers:
         amount = len(manager.shifts)
         tsheet.insert_rows(idx=2, amount=amount + 1)
@@ -130,11 +116,11 @@ def main(manager_file, yearInput, monthInput, teams_file):
                 tsheet['H' + str(row)] = '5. Pink'
 
     # Save and close Workbooks
-    printff(f"Saving {teams_file}\n")
+    
     wb1.save(teams_file)
 
-    printff(f"Closing {manager_file}\n")
+    
     wb.close()
 
-    printff(f"Closing {teams_file}\n")
+    
     wb1.close()
